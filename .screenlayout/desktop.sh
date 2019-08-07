e739779321e4d2e1e52aecd88c69029b685bdace
Sean McKenna <seanm@lucidchart.com>
2019-08-06 21:26:12 -0600

    Update monitor layout settings for work Ubuntu laptop since upgrading drivers

--- .screenlayout/desktop.sh
+++ .screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x240 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
