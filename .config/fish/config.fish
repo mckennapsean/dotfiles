c3e46297a7685aa07545c651221d326cfa76c019
Sean McKenna <mckennapsean@gmail.com>
2025-01-24 23:17:08 -0700

    feat: add basic fish settings and abbreviations

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+abbr -a -- g git
+abbr -a -- b lucid-bazel
+abbr -a -- lm 'cd ~/lucid/main/'
+abbr -a -- bfd 'lucid-bazel format --diff'
+abbr -a --set-cursor='%' -- bfs 'bazel format --diff --since=%'
+abbr -a -- check 'git check && git fetch origin master master-built master-stable && echo "Git fetched from upstream." && git check'
+abbr -a -- cpr 'shed bitbucket create-pr-link'
+abbr -a -- csd 'shed bitbucket create-pr-link --target $(git log --pretty=format:%D | grep -m1 "^origin/" | cut -d, -f1 | sed "s/^origin\///")'
+abbr -a -- pkk 'pkill -9 -f'
+abbr -a -- cpu 'watch grep \"cpu MHz\" /proc/cpuinfo'
