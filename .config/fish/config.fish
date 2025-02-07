8b56a6a4088efae5ae206e46de60dcae013bbaec
Sean McKenna <seanm@lucidchart.com>
2025-02-07 09:37:19 -0700

    feat: update check completion alias for jj

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ abbr -a -- b lucid-bazel
 abbr -a -- lm 'cd ~/lucid/main/'
 abbr -a -- bfd 'lucid-bazel format --diff'
 abbr -a --set-cursor='%' -- bfs 'bazel format --diff --since=%'
-abbr -a -- check 'git check && git fetch origin master master-built master-stable && echo "Git fetched from upstream." && git check'
+abbr -a -- check 'git check && jj git fetch --branch  master --branch master-stable && echo "Git fetched from upstream." && git check'
 abbr -a -- cpr 'shed bitbucket create-pr-link'
 abbr -a -- csd 'shed bitbucket create-pr-link --target $(git log --pretty=format:%D | grep -m1 "^origin/" | cut -d, -f1 | sed "s/^origin\///")'
 abbr -a -- pkk 'pkill -9 -f'
