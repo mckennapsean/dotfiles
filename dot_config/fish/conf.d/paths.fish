09c370622440be38add516b4f5090bc2f05df58e
Sean McKenna <mckennapsean@gmail.com>
2026-09-22 13:39:15 -0600

    feat: add portal user paths to fish
    Migrated from the old fish_variables to work across pcs.

---
+++ dot_config/fish/conf.d/paths.fish
@@ -0,0 +1,8 @@
+fish_add_path ~/.local/bin
+fish_add_path ~/.local/apps
+fish_add_path ~/.cargo/bin
+fish_add_path ~/go/bin
+fish_add_path ~/.local/share/coursier/bin
+fish_add_path ~/lucid/review-prompt
+fish_add_path ~/.local/lib/node_modules/@anthropic-ai/claude-code
+
