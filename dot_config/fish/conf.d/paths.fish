24a9f648d91395a5ada352c21159dd5299a2d6b9
Sean McKenna <mckennapsean@gmail.com>
2026-09-23 01:14:55 -0600

    fix: update go bin install to a hidden directory

--- dot_config/fish/conf.d/paths.fish
+++ dot_config/fish/conf.d/paths.fish
@@ -1,7 +1,7 @@
 fish_add_path -g ~/.local/bin
 fish_add_path -g ~/.local/apps
 fish_add_path -g ~/.cargo/bin
-fish_add_path -g ~/go/bin
+fish_add_path -g ~/.go/bin
 fish_add_path -g ~/.local/share/coursier/bin
 fish_add_path -g ~/lucid/review-prompt
 fish_add_path -g ~/.local/lib/node_modules/@anthropic-ai/claude-code
